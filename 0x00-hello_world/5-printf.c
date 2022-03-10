726aa00c3d625e8575882f16da97ff93db5ca939
Aysuarex <aysuarex@gmail.com>
2022-03-10 15:33:16 -0800

    5 recorrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Description: prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
- * /
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a pice of art, \n");
