9bd0ac93b0750e7148acfe7c9bb530600528daa0
Aysuarex <aysuarex@gmail.com>
2022-03-10 15:30:49 -0800

    5 amended

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main -Entry point
  *
- * Description: prints "with proper gramma, but the outcome is a piece of art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
- * */
+ * /
 int main(void)
 {
-	printf("with proper gramar, but the outcome is a pice of art, \n");
+	printf("with proper grammar, but the outcome is a pice of art, \n");
 	return (0);
 }
