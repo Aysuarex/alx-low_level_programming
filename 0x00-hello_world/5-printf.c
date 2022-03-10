363f600c07e0b9a640973e324dd1a8f55a76d779
Aysuarex <aysuarex@gmail.com>
2022-03-10 15:39:24 -0800

    5 again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
  * main -Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
- *
+ * Description: prints "with proper grammar, but the outcome is a piece
+ * of art,"
  * Return: Always 0 (Success)
  */
 int main(void)
