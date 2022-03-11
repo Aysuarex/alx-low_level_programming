5106495ffbc76de1b4593a6f8d19abec9b41c886
Aysuarex <aysuarex@gmail.com>
2022-03-10 16:41:10 -0800

    6 redone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - print the size of various types
- *
- * Description: prints the size of various types on the computer it
- * is compiled and run on
+ * main - Entry point
  *
  * Return : Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long
-				long int));
-	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 
 	return (0);
 }
