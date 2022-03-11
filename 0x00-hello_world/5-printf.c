d4c41d4fc9bffc7b6581165df70c07b124e6b818
Aysuarex <aysuarex@gmail.com>
2022-03-10 17:02:37 -0800

    typo fix2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
