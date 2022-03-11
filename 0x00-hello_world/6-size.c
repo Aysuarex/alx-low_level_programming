2400d75dbb5e41a39efed4257101e472b0b09496
Aysuarex <aysuarex@gmail.com>
2022-03-10 16:51:56 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -18,6 +17,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
-
 	return (0);
 }
