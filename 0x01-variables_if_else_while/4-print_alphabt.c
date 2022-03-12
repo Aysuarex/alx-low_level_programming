e464fe29ed703d48a8ecedabcc6f906cb85b3272
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-12 01:46:38 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Use 'putchar' to print all letters but the letter 'q' and 'e'.
+ * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
  * Return: 0
  */
 int main(void)
