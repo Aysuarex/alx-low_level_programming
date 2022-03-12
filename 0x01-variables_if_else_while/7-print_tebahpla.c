48e46f88335cfdceb2070a40ea4936eeaf0a2605
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-12 01:24:26 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,10 +3,11 @@
  * main - main block
  * Description: Print the alphabet in reverse order in lowercase.
  * Return: 0
- */int main(void)
- {
-	
+ */
+int main(void)
+{
 	char c;
+
 	for (c = 'z'; c >= 'a'; c--)
 	{
 		putchar(c);
