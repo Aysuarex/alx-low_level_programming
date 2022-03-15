6950823d887e5c7dc10cccb099ddb6e967d5b572
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-15 16:42:50 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,13 @@
-jdujdbj
+#include "main.h"
+
+/**
+ * add - Check Holberton
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
