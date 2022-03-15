e29d7969cffd58dc917ce5af6436490bf12aa398
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-15 16:23:02 -0700

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - print first 50 fibonacci
  *
- * Return: Always 0 (Success)
+ *Return: 0 always.
  */
-
 int main(void)
 {
-	int i;
-	long int fibonacci[50], sum = 2;
+	long int i, x = 1, y = 2, sum = 0, tSum = 0;
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-
-	for (i = 2; i < 50; i++)
+	for (i = 0; i < 49; i++)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
-			sum += fibonacci[i];
-	}
-	printf("%ld\n", sum);
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			tSum = tSum + y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
 
+	}
+	printf("%ld\n", tSum);
 	return (0);
 }
