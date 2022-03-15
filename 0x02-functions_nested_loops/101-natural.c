aeb00fcb081c8739f1b6eee0c17000090dc94a29
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-15 22:41:18 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,37 @@
-susjdbd
+#include <stdio.h>
+
+/**
+
+ *main - check for multiples of 3 and 5
+
+ *
+
+ *Return: 0 always
+
+ */
+
+int main(void)
+
+{
+
+int x = 1024, y, sum = 0;
+
+for (y = 0; y < x; y++)
+
+{
+
+if ((y % 3 == 0) || (y % 5 == 0))
+
+{
+
+sum = sum + y;
+
+}
+
+}
+
+printf("%d\n", sum);
+
+return (0);
+
+}
