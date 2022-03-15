5809d8a011dc8acad0c5d81bb117754bdb378221
Aysuarex <aysuarex@gmail.com>
2022-03-15 07:15:13 -0700

    new

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,1 @@
-#include "main.h"
-
-/**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
- */
-int _isalpha(int c)
-{
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-	{
-		return (1);
-	} else
-	{
-		return (0);
-	}
-
-}
+bdhfbfjfbfj
