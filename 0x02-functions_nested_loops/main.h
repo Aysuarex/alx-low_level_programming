c025ea4580676aa20eac1b59667395d934a6b2d3
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-15 18:20:40 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,29 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+int _putchar(char);
 
-/**
- * File: main.h
- * Auth: Bamidele Adefolaju
- * Desc: Header file containing declarations for  all functions
- *              used in the 0x02-functions_nested_loops directory
- *
- * _putchar - puts a charcters to be output.
- *
- * Description: prints a character to a standard out put
- *       as like putchar function.
- *
- * @c: an argument to be placeholder
- *
- * Return: different depend on its function to be retun.
- */
-
-int _putchar(char c);
 void print_alphabet(void);
+
 void print_alphabet_x10(void);
+
 int _islower(int c);
+
 int _isalpha(int c);
+
 int print_sign(int n);
-int _abs(int);
+
+int _abs(int n);
+
 int print_last_digit(int);
 
-#endif
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
