2b0ddb32280029b412b53d5c3e20e71343da440a
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-15 17:22:31 -0700

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,11 @@
-ndjfnjm
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
