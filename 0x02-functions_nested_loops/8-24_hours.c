d3460d5d3d8894f532ed72edb48b3758ce1b5389
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-15 17:46:16 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * jack_bauer - Check Holberton
- * Description: function prints every minute in one day
- * Return: Nothing
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
+int main(void)
 {
-	int H, M;
-
-	for (H = 0; H < 24; H++)
-	{
-		for (M = 0; M < 60; M++)
-		{
-			_putchar((H / 10) + '0');
-			_putchar((H % 10) + '0');
-			_putchar(':');
-			_putchar((M / 10) + '0');
-			_putchar((M % 10) + '0');
-			_putchar('\n');
-		}
-	}
+  jack_bauer();
+  return (0);
 }
