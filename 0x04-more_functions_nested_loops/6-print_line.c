3b29c63e091c435a7f4e10a688a6ce9a21760e0a
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-16 22:50:48 -0700

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,17 @@
-djjfndjfbj
+#include "holberton.h"
+
+/**
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
