4f21688b914eb29450e0b31ab098b703784f43fc
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-16 22:41:20 -0700

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,12 @@
-jdjfnfk
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
