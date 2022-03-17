c3eb160b9f1653706c3fbf97d5f0cebb3b4873d2
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-17 05:35:59 -0700

    Update holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,24 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
- */
-
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
+extern int _putchar(char c);
+extern int _isupper(int c);
+extern int _isdigit(int c);
+extern int mul(int a, int b);
+extern void print_numbers(void);
+extern void print_most_numbers(void);
+extern void more_numbers(void);
+extern void print_line(int n);
+extern void print_diagonal(int n);
+extern void print_square(int size);
+extern void print_triangle(int size);
+extern void print_number(int n);
 #endif
