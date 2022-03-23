29cdb224296fdfea9e2491800dd1eb8a750ce8b8
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-03-23 05:06:18 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,25 @@
-nhfjfb
+#include "main.h"
+/**
+ * _strcat -concatenates two strings,
+ * @dest: destination.
+ * @src: source
+ * Return: Pointer to dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count 2 >= 0)
+	{
+		*(dest + count) + *(src + count 2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return(dest);
+}
