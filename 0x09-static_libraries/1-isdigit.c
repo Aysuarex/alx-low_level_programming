ff98eac799f5538b938205a9950013d77d6021d2
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-04-01 11:46:46 -0700

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,11 @@
-jmfkfmk
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
