4753bdd9767edd7cbe4ad8799c72ebfb6206ba8f
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-04-01 14:55:46 -0700

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,14 @@
-udhnxorf,
+#include "main.h"
+
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
