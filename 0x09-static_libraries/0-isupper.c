4abaf91bc2a6c796563d93c9bb7803cebd2052da
Aysuarex <99369085+Aysuarex@users.noreply.github.com>
2022-04-01 14:48:30 -0700

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,1 +1,11 @@
-nrkfn
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
