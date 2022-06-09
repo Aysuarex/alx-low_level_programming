87e2ee8797bf66ccb9609198f8c942f316464192
Aysuarex <aysuarex@gmail.com>
2022-06-09 11:02:32 +0100

    Adding header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -30,4 +30,5 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
+
 #endif
