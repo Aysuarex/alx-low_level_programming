2959d6b9baa2f7f22dfcd06fd6add62e50cd1234
Aysuarex <aysuarex@gmail.com>
2022-06-14 00:33:38 +0100

    Driver file for task 2

---
+++ 0x18-dynamic_libraries/1-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+int mul(int a, int b)
+{
+    return a * b;
+}
+int div(int a, int b)
+{
+    return a / b;
+}
+int mod(int a, int b)
+{
+    return a % b;
+}
