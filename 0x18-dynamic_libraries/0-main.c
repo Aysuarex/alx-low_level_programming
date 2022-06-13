82e1e089bba1ecc96bf816064e87b557a6666732
Aysuarex <aysuarex@gmail.com>
2022-06-13 23:54:17 +0100

    Task 0 driver function

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * 
+ * Return: Always EXIT_SUCCESS;
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn lib"));
+    return (EXIT_SUCCESS);
+}
