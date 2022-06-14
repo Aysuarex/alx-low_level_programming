9ddf976536615abfed9d55d5ef58e29c7dc16651
Aysuarex <aysuarex@gmail.com>
2022-06-14 01:16:33 +0100

    Fixing url

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-wget -P /tmp/ https://raw.github.com/aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so 
+wget -P /tmp/ https://raw.githubusercontent.com/aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so 
 export LD_PRELOAD=/tmp/nrandom.so
