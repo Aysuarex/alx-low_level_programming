450a3e63a9e036996dfe437768cf2113445a25f4
Aysuarex <aysuarex@gmail.com>
2022-07-01 00:39:30 +0100

    Mandatory Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * Return: value of the hash.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int k_index;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return (NULL);
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[k_index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
+}
