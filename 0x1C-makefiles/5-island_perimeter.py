2971599e069ec50583db1f97ef1a05a7112e49fd
Aysuarex <aysuarex@gmail.com>
2022-08-17 23:07:50 +0100

    python file for task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module that contains the island_perimeter function """
+
+
+def island_perimeter(grid):
+    """ Function that returns the perimeter of island described in grid """
+    perimeter = 0
+
+    nrows = len(grid)
+
+    if grid != []:
+        ncolumns = len(grid[0])
+
+    for a in range(nrows):
+        for b in range(ncolumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
