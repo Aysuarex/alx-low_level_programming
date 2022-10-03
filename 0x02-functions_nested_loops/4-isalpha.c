8c469f129897dd8160ce532bc28301253e7210f4
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:06:26 +0100

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _isalpha - Check Holberton
+ * _isalpha - Check Main
  * @c: An input character
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
