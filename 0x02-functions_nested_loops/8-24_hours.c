007667aaa334187ac948e36fe7eac219e84aaeae
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:07:00 +0100

    Hours of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - Check Holberton
+ * jack_bauer - Check Main
  * Description: function prints every minute in one day
  * Return: Nothing
  */
