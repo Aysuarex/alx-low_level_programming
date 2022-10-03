767a8fd5566b1128e39b71240346e58e500f1bee
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:03:13 +0100

    Manndatory task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX students.
  * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Always 0.
