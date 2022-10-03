8bbc270a2f5d2b785e5c8d616566035143b58254
Aysuarex <aysuarex@gmail.com>
2022-10-03 18:59:57 +0100

    Updated header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,14 @@
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int);
+int _abs(int r);
+int print_last_digit(int r);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void putformat(int);
