c06bdc3726fc8f5043c0f631a292e41235e4db74
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:06:11 +0100

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - Check Holberton
+ * print_to_98 - Check Main
  * @n: A input integer
  * Description: function that prints all natural numbers
  * from n to 98, followed by a new line.
