06d928559c9c588461e3ecc051f77a1b62172983
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:02:36 +0100

    file for advanced task 0

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX students.
  * @argc: argument count.
  * @argv: argument vector.
  *
