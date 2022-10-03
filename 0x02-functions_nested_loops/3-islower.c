2cd9f30c4ea60ff28998832b41658f5e0d594b44
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:04:47 +0100

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Check Holberton
+ * _islower - Check Main
  * @c: An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
