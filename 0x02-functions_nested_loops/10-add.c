9321d52f6202396c839b137de70d48d44be7804a
Aysuarex <aysuarex@gmail.com>
2022-10-03 19:07:26 +0100

    Task 10 file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - Check Holberton
+ * add - Check Main
  * @a: An integer a
  * @b: An integer b
  * Description: Function that adds two integers and returns the result
